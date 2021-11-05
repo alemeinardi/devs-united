2c03e7a3ee9450bf2fcb56008a6d0313d21d651c
Ale Meinardi <ameinardi@gmail.com>
2021-11-05 08:58:48 -0300

    Tweeter changes. Date of tweets. Content counter

--- src/components/Tweeter/index.js
+++ src/components/Tweeter/index.js
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import styles from "./index.module.css";
 import { AppContext } from "../../contexts/AppContext.js";
 import { myfirestore } from "../../firebase/firebase.js"
 
 const Tweeter = () => {
+  const [longTweet, setLongTweet] = useState(0);
   const { tweet, setTweet } = useContext(AppContext);
 
   const handleChange = (e) => {
     let newTweet = {...tweet,
     [e.target.name]: e.target.value}
-    console.log(newTweet)
+    newTweet.date = new Date(Date.now())
+    e.target.name === "tweet" && setLongTweet(e.target.value.length)
     setTweet(newTweet)
   }
 
@@ -22,7 +24,7 @@ const Tweeter = () => {
     <textarea name="tweet" placeholder="What’s happening?" cols="60" rows="6" onChange={handleChange}>
     </textarea>
     <div className={styles.letters}>
-      <span className={styles.letter_counter}>0</span>
+      <span className={styles.letter_counter}>{longTweet}</span>
       <span className={styles.letter_max}>200 max.</span>
     </div>
     <button onClick={sendTweet}>POST</button>
